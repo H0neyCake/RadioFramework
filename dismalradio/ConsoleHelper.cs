eaebdd815adfdfb5f7c2caa039fe5b9b57f692f0
H0neyCake <dimex4@ya.ru>
2019-04-23 14:15:48 +0300

    Initial commit

---
+++ dismalradio/ConsoleHelper.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dismalradio
+{
+    class ConsoleHelper
+    {
+        public static void CreateConsole()
+        {
+            AllocConsole();
+
+            // stdout's handle seems to always be equal to 7
+            var defaultStdout = new IntPtr(7);
+            var currentStdout = GetStdHandle(StdOutputHandle);
+
+            if (currentStdout != defaultStdout)
+                // reset stdout
+                SetStdHandle(StdOutputHandle, defaultStdout);
+
+            // reopen stdout
+            TextWriter writer = new StreamWriter(Console.OpenStandardOutput()) { AutoFlush = true };
+            Console.SetOut(writer);
+        }
+
+        // P/Invoke required:
+        private const UInt32 StdOutputHandle = 0xFFFFFFF5;
+        [DllImport("kernel32.dll")]
+        private static extern IntPtr GetStdHandle(UInt32 nStdHandle);
+        [DllImport("kernel32.dll")]
+        private static extern void SetStdHandle(UInt32 nStdHandle, IntPtr handle);
+        [DllImport("kernel32")]
+        static extern bool AllocConsole();
+    }
+}
